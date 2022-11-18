5651a03b01eba507ee117606459ddabd5b36b311
SEBASTIAN KUMAR <s-SKUMAR@lwsd.org>
2022-11-18 13:27:25 -0800

    Made some changes with Steve's help.

--- minimalist-game-framework-core/Game/RenderEngine.cs
+++ minimalist-game-framework-core/Game/RenderEngine.cs
@@ -7,32 +7,34 @@ namespace Mooyash.Services
     {
         public Vector2 position;
         public double angle; //0 = positive x, pi/2 = positive y
-        public double height;
+        public float height;
         public double hfov; //in radians
-        public double screen; //how far (in cm) the screen is in front of camera
+        public float screen; //how far (in cm) the screen is in front of camera
     }
 
     public static class RenderEngine
     {
         public static Camera camera;
 
-        public static Vector2 convert(int x, int y)
+        public static Vector2 rotate(Vector2 input)
         {
             Vector2 temp;
             Vector2 result;
             //convert x,y to be relative to camera
-            temp.X = x - camera.position.X;
-            temp.Y = y - camera.position.Y;
+            temp.X = input.X - camera.position.X;
+            temp.Y = input.Y - camera.position.Y;
             //convert coordinate system (+y is away from camera)
             result.X = Vector2.Dot(new Vector2(-(float)Math.Sin(camera.angle), (float)Math.Cos(camera.angle)), temp);
-            result.Y = Vector2.Dot(new Vector2( (float)Math.Cos(camera.angle), (float)Math.Sin(camera.angle)), temp);
-            //project coordinates onto screen
+            result.Y = Vector2.Dot(new Vector2((float)Math.Cos(camera.angle), (float)Math.Sin(camera.angle)), temp);
+            return result;
+        }
 
-            //WHAT IF RESULT.Y IS TINY?!
-            result.X = (float)  camera.screen * result.X / result.Y;
-            result.Y = (float) -camera.screen * (float) camera.height / result.Y;
-            System.Diagnostics.Debug.WriteLine(result.X);
-            System.Diagnostics.Debug.WriteLine(result.Y);
+        public static Vector2 project(Vector2 input)
+        {
+            Vector2 result = new Vector2();
+            //project coordinates onto screen
+            result.X =  camera.screen * input.X / input.Y;
+            result.Y = -camera.screen * camera.height / input.Y;
             //scale according to FOV
             float scale = Game.Resolution.X / (float) (2 * camera.screen * Math.Tan(camera.hfov / 2));
             result.X = result.X * scale;
@@ -41,6 +43,7 @@ namespace Mooyash.Services
             result.X += Game.Resolution.X / 2;
             result.Y = Game.Resolution.Y - result.Y;
             return result;
+
             /*
             result.Y = (float) Math.Min(Game.Resolution.Y-camera.horizon,camera.scale * (x*Math.Cos(camera.angle) + y*Math.Sin(camera.angle)));
             result.X = (float) (camera.scale * (-x*Math.Sin(camera.angle) + y*Math.Cos(camera.angle)) *
@@ -57,11 +60,19 @@ namespace Mooyash.Services
             bool draw = false;
             for(int i = 0; i < points.Length; i++)
             {
-                points[i] = convert(p.points[i].x, p.points[i].y);
+                p.points[i] = rotate(p.points[i]);
+            }
+            if(!p.splice())
+            {
+
+            }
+            for(int i = 0; i < points.Length; i++)
+            {
+                p.points[i] = convert(p.points[i]);
             }
             if(draw)
             {
-                Engine.DrawConvexPolygon(new Polygon(points, p.color));
+                Engine.DrawConvexPolygon(new Polygon(p.points, p.color));
             }
         }
 
