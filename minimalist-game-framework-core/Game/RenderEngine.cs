7a4d9ffd29f64641a6f57f48203004d135a936f3
SEBASTIAN KUMAR <s-SKUMAR@lwsd.org>
2022-11-18 20:44:35 -0800

    Added checks for drawing and splicing.

--- minimalist-game-framework-core/Game/RenderEngine.cs
+++ minimalist-game-framework-core/Game/RenderEngine.cs
@@ -6,10 +6,28 @@ namespace Mooyash.Services
     public class Camera
     {
         public Vector2 position;
-        public double angle; //0 = positive x, pi/2 = positive y
         public float height;
-        public double hfov; //in radians
-        public float screen; //how far (in cm) the screen is in front of camera
+        public double hfov { get; } //in radians
+        public double angle { get; } //0 = positive x, pi/2 = positive y
+        public float screen { get; } //how far (in cm) the screen is in front of camera
+        public float sin { get; } // of angle
+        public float cos { get; } // of angle
+        public float scale { get; } //based on hfov and screen
+        public float slope { get; } //for handling drawing conditions
+
+        public Camera(Vector2 position, double angle, float height, double hfov, float screen)
+        {
+            this.position = position;
+            this.angle = angle;
+            this.height = height;
+            this.hfov = hfov;
+            this.screen = screen;
+
+            sin = (float) Math.Sin(angle);
+            cos = (float) Math.Cos(angle);
+            slope = (float) Math.Tan(hfov / 2);
+            scale = Game.Resolution.X / (float)(2 * screen * slope);
+        }
     }
 
     public static class RenderEngine
@@ -24,8 +42,8 @@ namespace Mooyash.Services
             temp.X = input.X - camera.position.X;
             temp.Y = input.Y - camera.position.Y;
             //convert coordinate system (+y is away from camera)
-            result.X = Vector2.Dot(new Vector2(-(float)Math.Sin(camera.angle), (float)Math.Cos(camera.angle)), temp);
-            result.Y = Vector2.Dot(new Vector2((float)Math.Cos(camera.angle), (float)Math.Sin(camera.angle)), temp);
+            result.X = Vector2.Dot(new Vector2(-camera.sin, camera.cos), temp);
+            result.Y = Vector2.Dot(new Vector2(camera.cos, camera.sin), temp);
             return result;
         }
 
@@ -36,9 +54,8 @@ namespace Mooyash.Services
             result.X = camera.screen * input.X / input.Y;
             result.Y = -camera.screen * camera.height / input.Y;
             //scale according to FOV
-            float scale = Game.Resolution.X / (float)(2 * camera.screen * Math.Tan(camera.hfov / 2));
-            result.X = result.X * scale;
-            result.Y = result.Y * scale;
+            result.X = result.X * camera.scale;
+            result.Y = result.Y * camera.scale;
             //convert to MGF coordinate system
             result.X += Game.Resolution.X / 2;
             result.Y = Game.Resolution.Y - result.Y;
@@ -56,28 +73,31 @@ namespace Mooyash.Services
 
         public static void drawPerPolygon(Polygon p)
         {
-            Polygon temp = new Polygon(p.points);
-            //Vector2[] points = new Vector2[p.points.Length];
-            bool draw = true;
-            bool splice = true;
-            //check whether polygon should be drawn (return if shouldn't) or spliced
-            if(!draw) { return; }
+            Polygon temp = new Polygon(p.points, p.color);
+            bool draw = false;
+            bool splice = false;
+            bool inside;
             for (int i = 0; i < temp.points.Length; i++)
             {
-                temp.points[i] = rotate(p.points[i]);
+                temp.points[i] = rotate(temp.points[i]);
+                inside = (temp.points[i].Y > camera.screen) &&
+                    (camera.slope * temp.points[i].Y + temp.points[i].X > 0) &&
+                    (camera.slope * temp.points[i].Y - temp.points[i].X > 0);
+                draw = (draw || inside);
+                splice = (splice || !inside);
             }
+            if(!draw) { return; }
             if (splice)
             {
                 temp.splice();
             }
-            for (int i = 0; i < points.Length; i++)
+            for (int i = 0; i < temp.points.Length; i++)
             {
                 temp.points[i] = project(temp.points[i]);
             }
             Engine.DrawConvexPolygon(temp);
         }
 
-        //don't draw everything in the track
         public static void drawPerTrack(Track t)
         {
             foreach (Polygon p in t.interactable)
