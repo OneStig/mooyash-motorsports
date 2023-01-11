fbde26f20828a6e2e26f7f8709b06f7d6d23d9eb
Steven He <s-sthe@lwsd.org>
2023-01-10 21:31:40 -0800

    Deleting duplicate files

--- minimalist-game-framework-core/Game/Coin.cs
+++ minimalist-game-framework-core/Game/Coin.cs
@@ -6,35 +6,28 @@ namespace Mooyash.Modules
 {
     public class Coin : GameObject
     {
-        
         public Coin(Vector2 position) : base()
         {
             this.position = position;
             texture = Engine.LoadTexture("temp.png");
             size = new Vector2(400, 400);
             resolution = new Vector2(32, 32);
-            numTex = 1;
+
             radius = 40f;
         }
 
 
-        public void update()
+        public void update(float dt)
         {
-            
-           
+
         }
 
 
         public override void collide(Kart k)
         {
-            if(RenderEngine.score == 0)
-            {
-                k.score = 0;
-            }
             k.score += 1;
-            
-            RenderEngine.score = k.score;
-            
+            //Engine.DrawString("score " + k.score, new Vector2(100, 5), Color.White, );
+
             PhysicsEngine.gameObjects.Remove(this);
         }
     }
