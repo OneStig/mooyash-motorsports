a7b392a24a6e3958ab4d62624e937bbc9df1e51d
Davis Yang <s-dyang@lwsd.org>
2023-01-16 20:15:37 -0800

    Updated Menu System (Should work with new font)

--- minimalist-game-framework-core/Game/MenuSystem.cs
+++ minimalist-game-framework-core/Game/MenuSystem.cs
@@ -22,35 +22,34 @@ namespace Mooyash.Services
             Texture MenuWithoutLogo = Engine.LoadTexture("fallinggong.png");
             Texture CreditsScreen = Engine.LoadTexture("Credits.png");
 
-            SettingtoID["back"] = 0;
+            SettingtoID["Back"] = 0; 
+            SettingtoID["Play"] = 0;
 
-            SettingtoID["play"] = 0;
+            SettingtoID["Time Trial"] = 0;
+            SettingtoID["Grand Prix"] = 1;
 
-            SettingtoID["timetrial"] = 0;
-            SettingtoID["grandprix"] = 1;
+            SettingtoID["50CC"] = 0;
+            SettingtoID["100CC"] = 1;
 
-            SettingtoID["50cc"] = 0;
-            SettingtoID["100cc"] = 1;
-
-            SettingtoID["william"] = 0;
-            SettingtoID["suyash"] = 1;
+            SettingtoID["William"] = 0;
+            SettingtoID["Suyash"] = 1;
 
             SettingtoID["map1"] = 0;
             SettingtoID["map2"] = 1;
             SettingtoID["map3"] = 2;
 
-            SettingtoID["replay"] = 0;
-            SettingtoID["return"] = 1;
-            SettingtoID["credits"] = 2;
+            SettingtoID["Replay"] = 0;
+            SettingtoID["Return"] = 1;
+            SettingtoID["Credits"] = 2;
 
             //play mode cc character map return
 
             //play
             Texture[] MenuTextures = new Texture[] { Menu };
-            Vector2[] MenuTexturePositions = new Vector2[] { new Vector2(0, 0)};
-            Vector2[] MenuTextureSizes = new Vector2[] { new Vector2(320, 180)};
+            Vector2[] MenuTexturePositions = new Vector2[] { new Vector2(0, 0) };
+            Vector2[] MenuTextureSizes = new Vector2[] { new Vector2(320, 180) };
             Dictionary<int, Button> MenuButtons = new Dictionary<int, Button>();
-            MenuButtons[0] = new Button(Color.Black, new Vector2(120, 90), new Vector2(80, 30), "play", Color.White);
+            MenuButtons[0] = new Button(Color.Black, new Vector2(120, 90), new Vector2(80, 30), "Play", Color.White);
             ScreenStack[0] = new Screen(MenuTextures, MenuTexturePositions, MenuTextureSizes, MenuButtons, 0);
 
             //mode
@@ -58,10 +57,9 @@ namespace Mooyash.Services
             Vector2[] GamemodeTexturePositions = new Vector2[] { new Vector2(0, 0) };
             Vector2[] GamemodeTextureSizes = new Vector2[] { new Vector2(320, 180) };
             Dictionary<int, Button> GamemodeButtons = new Dictionary<int, Button>();
-            GamemodeButtons[0] = new Button(Color.Black, new Vector2(45, 60), new Vector2(100, 30), "timetrial", Color.White);
-            GamemodeButtons[1] = new Button(Color.Black, new Vector2(175, 60), new Vector2(100, 30), "grandprix", Color.White);
-
-            GamemodeButtons[2] = new Button(Color.Black, new Vector2(125, 100), new Vector2(100, 30), "back", Color.White); 
+            GamemodeButtons[0] = new Button(Color.Black, new Vector2(45, 60), new Vector2(100, 30), "Time Trial", Color.White);
+            GamemodeButtons[1] = new Button(Color.Black, new Vector2(175, 60), new Vector2(100, 30), "Grand Prix", Color.White);
+            GamemodeButtons[2] = new Button(Color.Black, new Vector2(160, 120), new Vector2(50, 25), "Back", Color.White);
             ScreenStack[1] = new Screen(GamemodeTextures, GamemodeTexturePositions, GamemodeTextureSizes, GamemodeButtons, 0);
 
             //cc
@@ -69,10 +67,9 @@ namespace Mooyash.Services
             Vector2[] CCTexturePositions = new Vector2[] { new Vector2(0, 0) };
             Vector2[] CCTextureSizes = new Vector2[] { new Vector2(320, 180) };
             Dictionary<int, Button> CCButtons = new Dictionary<int, Button>();
-            CCButtons[0] = new Button(Color.Black, new Vector2(76, 60), new Vector2(75, 30), "50cc", Color.White);
-            CCButtons[1] = new Button(Color.Black, new Vector2(176, 60), new Vector2(75, 30), "100cc", Color.White);
-
-            CCButtons[2] = new Button(Color.Black, new Vector2(125, 100), new Vector2(100, 30), "back", Color.White);
+            CCButtons[0] = new Button(Color.Black, new Vector2(76, 60), new Vector2(75, 30), "50CC", Color.White);
+            CCButtons[1] = new Button(Color.Black, new Vector2(176, 60), new Vector2(75, 30), "100CC", Color.White);
+            CCButtons[2] = new Button(Color.Black, new Vector2(160, 120), new Vector2(50, 25), "Back", Color.White);
             ScreenStack[2] = new Screen(CCTextures, CCTexturePositions, CCTextureSizes, CCButtons, 0);
 
             //character
@@ -80,10 +77,9 @@ namespace Mooyash.Services
             Vector2[] CharacterTexturePosition = new Vector2[] { new Vector2(0, 0) };
             Vector2[] CharacterTextureSizes = new Vector2[] { new Vector2(320, 180) };
             Dictionary<int, Button> CharacterButtons = new Dictionary<int, Button>();
-            CharacterButtons[0] = new Button(Color.Black, new Vector2(76, 60), new Vector2(75, 30), "william", Color.White);
-            CharacterButtons[1] = new Button(Color.Black, new Vector2(176, 60), new Vector2(75, 30), "suyash", Color.White);
-
-            CharacterButtons[2] = new Button(Color.Black, new Vector2(125, 100), new Vector2(100, 30), "back", Color.White);
+            CharacterButtons[0] = new Button(Color.Black, new Vector2(76, 60), new Vector2(75, 30), "William", Color.White);
+            CharacterButtons[1] = new Button(Color.Black, new Vector2(176, 60), new Vector2(75, 30), "Suyash", Color.White);
+            CharacterButtons[2] = new Button(Color.Black, new Vector2(160, 120), new Vector2(50, 25), "Back", Color.White);
             ScreenStack[3] = new Screen(CharacterTextures, CharacterTexturePosition, CharacterTextureSizes, CharacterButtons, 0);
 
             //map
@@ -94,22 +90,19 @@ namespace Mooyash.Services
             MapButtons[0] = new Button(Color.Black, new Vector2(22, 60), new Vector2(75, 30), "map1", Color.White);
             MapButtons[1] = new Button(Color.Black, new Vector2(122, 60), new Vector2(75, 30), "map2", Color.White);
             MapButtons[2] = new Button(Color.Black, new Vector2(222, 60), new Vector2(75, 30), "map3", Color.White);
-
-            MapButtons[3] = new Button(Color.Black, new Vector2(125, 100), new Vector2(100, 30), "back", Color.White);
+            MapButtons[3] = new Button(Color.Black, new Vector2(160, 120), new Vector2(50, 25), "Back", Color.White);
 
             ScreenStack[4] = new Screen(MapTextures, MapTexturePositions, MapTextureSizes, MapButtons, 0);
 
             //return
-            Texture[] EndTextures = new Texture[] {  };
-            Vector2[] EndTexturePositions = new Vector2[] {  };
-            Vector2[] EndTextureSizes = new Vector2[] {  };
+            Texture[] EndTextures = new Texture[] { };
+            Vector2[] EndTexturePositions = new Vector2[] { };
+            Vector2[] EndTextureSizes = new Vector2[] { };
             Dictionary<int, Button> EndButtons = new Dictionary<int, Button>();
-            EndButtons[0] = new Button(Color.Black, new Vector2(76, 60), new Vector2(75, 30), "replay", Color.White);
-            EndButtons[1] = new Button(Color.Black, new Vector2(176, 60), new Vector2(75, 30), "return", Color.White);
-            EndButtons[2] = new Button(Color.Black, new Vector2(125, 100), new Vector2(75, 30), "credits", Color.White);
-                //score
-                //EndButtons[3] = new Button(new Color(0,0,0,0), new Vector2(152.5f, 30), new Vector2(20, 20), "", Color.White);
-            
+            EndButtons[0] = new Button(Color.Black, new Vector2(76, 60), new Vector2(75, 30), "Replay", Color.White);
+            EndButtons[1] = new Button(Color.Black, new Vector2(176, 60), new Vector2(75, 30), "Return", Color.White);
+            EndButtons[2] = new Button(Color.Black, new Vector2(125, 100), new Vector2(75, 30), "Credits", Color.White);
+
             ScreenStack[5] = new Screen(EndTextures, EndTexturePositions, EndTextureSizes, EndButtons, 0);
 
             //credits
@@ -117,10 +110,13 @@ namespace Mooyash.Services
             Vector2[] CreditTexturePositions = new Vector2[] { new Vector2(0, 0) };
             Vector2[] CreditTextureSizes = new Vector2[] { new Vector2(320, 180) };
             Dictionary<int, Button> CreditButtons = new Dictionary<int, Button>();
-            CreditButtons[0] = new Button(Color.Black, new Vector2(76, 140), new Vector2(75, 30), "replay", Color.White);
-            CreditButtons[1] = new Button(Color.Black, new Vector2(176, 140), new Vector2(75, 30), "return", Color.White);
+            CreditButtons[0] = new Button(Color.Black, new Vector2(76, 140), new Vector2(75, 30), "Replay", Color.White);
+            CreditButtons[1] = new Button(Color.Black, new Vector2(176, 140), new Vector2(75, 30), "Return", Color.White);
+            CreditButtons[2] = new Button(Color.Black, new Vector2(160, 120), new Vector2(50, 25), "Back", Color.White);
+
 
             ScreenStack[6] = new Screen(CreditTextures, CreditTexturePositions, CreditTextureSizes, CreditButtons, 0);
+
 
 
         }
@@ -139,7 +135,14 @@ namespace Mooyash.Services
 
             if (CurScreen == 5)
             {
-                Engine.DrawString(finTime, new Vector2(163, 33) * Game.ResolutionScale, Color.White, Game.font,TextAlignment.Center);
+                if (GetSettings()[1] == 0)
+                {
+                    Engine.DrawString("Time: " + finTime, new Vector2(163, 33) * Game.ResolutionScale, Color.White, Game.font, TextAlignment.Center);
+                }
+                else
+                {
+                    Engine.DrawString(finTime, new Vector2(163, 33) * Game.ResolutionScale, Color.White, Game.font, TextAlignment.Center);
+                }
             }
 
             if (Engine.GetKeyDown(Key.W) || Engine.GetKeyDown(Key.Up) || Engine.GetKeyDown(Key.Left) || Engine.GetKeyDown(Key.A))
@@ -155,23 +158,23 @@ namespace Mooyash.Services
                 if (CurScreen >= 5)
                 {
                     String select = cur.Select();
-                    if (select.Equals("replay"))
+                    if (select.Equals("Replay"))
                     {
                         CurScreen = 4;
                         Settings.RemoveAt(Settings.Count - 1);
                     }
-                    if (select.Equals("return"))
+                    if (select.Equals("Return"))
                     {
                         Settings.Clear();
                         CurScreen = 0;
                         Settings.Clear();
                     }
-                    if (select.Equals("credits"))
+                    if (select.Equals("Credits"))
                     {
                         CurScreen = 6;
                     }
                 }
-                else if (CurScreen < 5 && !cur.Select().Equals("back"))
+                else if (CurScreen < 5 && !cur.Select().Equals("Back"))
                 {
                     Settings.Add(cur.Select());
                     CurScreen++;
@@ -194,9 +197,11 @@ namespace Mooyash.Services
                     return true; //create new way to move on
                 }
             }
-            if (Engine.GetKeyDown(Key.Escape) && CurScreen > 0)
+            if (Engine.GetKeyDown(Key.Backspace) && CurScreen > 0)
             {
                 CurScreen--;
+                Settings.RemoveAt(Settings.Count - 1);
+                ScreenStack[CurScreen].curButton = 0;
             }
             return false;
         }
